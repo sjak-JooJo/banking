3e11bf811d93a7782b20f9fcc8646427e8797583
JooJo <sjak0101@gmail.com>
2022-05-24 15:51:44 +0100

    update balance

--- js/banking.js
+++ js/banking.js
@@ -23,3 +23,32 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     // clear the deposit input field
     depositInput.value = ' ';
 });
+
+// hande withdraw evenhandler
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    // get the withdraw ammount
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawText);
+
+    // set withdraw total
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+    
+    // clear withdraw input
+    withdrawInput.value = ' ';
+})
